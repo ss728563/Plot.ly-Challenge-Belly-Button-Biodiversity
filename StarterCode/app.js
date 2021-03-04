b3772b6409b2218dceae81b69eddb4b0ee751901
ss728563 <71289207+ss728563@users.noreply.github.com>
2021-03-04 16:52:51 -0500

    update

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,101 +1,141 @@
 // Belly Button Biodiversity - Plotly.js
 
-function buildMetadata(sample) {
+fvar data;
 
-    // @TODO: Complete the Following Function that Builds the Metadata Panel
-  
-    // Use `d3.json` to Fetch the Metadata for a Sample
-      d3.json(`/metadata/${sample}`).then((data) => {
-          // Use d3 to Select the Panel with id of `#sample-metadata`
-          var PANEL = d3.select("#sample-metadata");
-          // Use `.html("") to Clear any Existing Metadata
-          PANEL.html("");
-          // Use `Object.entries` to Add Each Key & Value Pair to the Panel
-          // Hint: Inside the Loop, Use d3 to Append New Tags for Each Key-Value in the Metadata
-          Object.entries(data).forEach(([key, value]) => {
-            PANEL.append("h6").text(`${key}:${value}`);
-          })
-          // BONUS: Build the Gauge Chart
-            buildGauge(data.WFREQ);
-      })
-  }
-  
-  function buildCharts(sample) {
-  
-    // @TODO: Use `d3.json` to Fetch the Sample Data for the Plots
-    d3.json(`/samples/${sample}`).then((data) => {
-      // @TODO: Build a Bubble Chart Using the Sample Data
-      const otu_ids = data.otu_ids;
-      const otu_labels = data.otu_labels;
-      const sample_values = data.sample_values;
-      // @TODO: Build a Pie Chart
-      let bubbleLayout = {
-        margin: { t: 0 },
-        hovermode: "closests",
-        xaxis: { title: "OTU ID"}
-      }
-  
-      let bubbleData = [
-        {
-          x: otu_ids,
-          y: sample_values,
-          text: otu_labels,
-          mode: "markers",
-          marker: {
-            size: sample_values,
-            color: otu_ids,
-            colorscale: "Earth"
-          }
-        }
-      ]
-  
-      Plotly.plot("bubble", bubbleData, bubbleLayout);
-  
-      // HINT: Use slice() to Grab the Top 10 sample_values,
-      // otu_ids, and otu_labels (10 Each)
-      let pieData = [
-        {
-          values: sample_values.slice(0, 10),
-          labels: otu_ids.slice(0, 10),
-          hovertext: otu_labels.slice(0, 10),
-          hoverinfo: "hovertext",
-          type: "pie"
-        }
-      ];
-      
-      let pieLayout = {
-        margin: { t: 0, l: 0 }
-      };
-  
-      Plotly.plot("pie", pieData, pieLayout)
-  })
-  }
-  
-  function init() {
-    // Grab a Reference to the Dropdown Select Element
-    var selector = d3.select("#selDataset");
-  
-    // Use the List of Sample Names to Populate the Select Options
-    d3.json("/names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-  
-      // Use the First Sample from the List to Build Initial Plots
-      const firstSample = sampleNames[0];
-      buildCharts(firstSample);
-      buildMetadata(firstSample);
+//init function to fill in the select option
+
+function init() {
+  d3.json("data/samples.json").then(incomingData => {
+    data = incomingData;
+    var selectValues = incomingData.names;
+    var selectOption = d3.select("#selDataset");
+
+    selectValues.forEach(value => {
+      selectOption
+        .append("option")
+        .text(value)
+        .attr("value", function() {
+          return value;
+        });
     });
+    d3.selectAll("#selDataset").on("change", plotFunctions);
+    d3.select('#selDataset').property('value', 940);
+  
+    plotFunctions();
+  });
+};
+
+//functions to clean and create bacteria/ouid lists
+function fixBacteria(name) {
+    var listBacteria = [];
+  
+    for (var i = 0; i < name.length; i++) {
+      var stringName = name[i].toString();
+      var splitName = stringName.split(";");
+      if (splitName.length > 1) {
+        listBacteria.push(splitName[splitName.length - 1]);
+      } else {
+        listBacteria.push(splitName[0]);
+      }
+    }
+    return listBacteria;
   }
   
-  function optionChanged(newSample) {
-    // Fetch New Data Each Time a New Sample is Selected
-    buildCharts(newSample);
-    buildMetadata(newSample);
+  function fixOuid(name) {
+    var listOuid = [];
+    for (var i = 0; i < name.length; i++) {
+      listOuid.push(`OTU ${name[i]}`);
+    }
+    return listOuid;
   }
-  
-  // Initialize the Dashboard
-  init();
+
+
+function plotFunctions() {
+  var valueSelect = d3.select("#selDataset").node().value;
+  console.log(valueSelect);
+  demographicInfo(valueSelect);
+  barChart(valueSelect);
+  bubbleChart(valueSelect);
+  gaugeChart(valueSelect);
+};
+
+function demographicInfo(valueSelect) {
+    var filteredValue = data.metadata.filter(d => d.id == +valueSelect);
+
+    var panel = d3.select(".panel-body");
+    panel.html("");
+    panel.append("p").text(`ID: ${filteredValue[0].id}`);
+    panel.append("p").text(`Ethnicity: ${filteredValue[0].ethnicity}`);
+    panel.append("p").text(`Gender: ${filteredValue[0].gender}`);
+    panel.append("p").text(`Age: ${filteredValue[0].age}`);
+    panel.append("p").text(`Location: ${filteredValue[0].location}`);
+    panel.append("p").text(`Bbtype: ${filteredValue[0].bbtype}`);
+    panel.append("p").text(`Wfreq: ${filteredValue[0].wfreq}`);
+    }
+
+function barChart(valueSelect) {
+  var filteredValue = data.samples.filter(d => d.id == +valueSelect);
+  var ouid = filteredValue.map(d => d.otu_ids);
+  ouid = fixOuid(ouid[0].slice(0, 10));
+  var valueX = filteredValue.map(d => d.sample_values);
+  valueX = valueX[0].slice(0, 10);
+
+  var otu_label = filteredValue.map(d => d.otu_labels);
+  var names = fixBacteria(otu_label[0]).slice(0, 10);
+  console.log(ouid);
+  console.log(valueX);
+  console.log(otu_label);
+  console.log(names);
+
+  var trace = {
+    x: valueX,
+    y: ouid,
+    text: names,
+    type: "bar",
+    orientation: "h"
+  };
+
+  var layout = {
+    yaxis: {
+      autorange: "reversed"
+    }
+  };
+
+  var barData = [trace];
+
+  Plotly.newPlot("bar", barData, layout);
+}
+
+
+function bubbleChart(valueSelect) {
+  var filteredValue = data.samples.filter(value => value.id == +valueSelect);
+  var ouid = filteredValue.map(d => d.otu_ids);
+  ouid = ouid[0];
+  var yValue = filteredValue.map(d => d.sample_values);
+  yValue = yValue[0];
+
+  var otu_label = filteredValue.map(d => d.otu_labels);
+  otu_label = fixBacteria(otu_label[0]);
+
+  var trace1 = {
+    x: ouid,
+    y: yValue,
+    mode: "markers",
+    marker: {
+      color: ouid,
+      size: yValue
+    },
+    text: otu_label
+  };
+
+  var bubbleData = [trace1];
+
+  var layout = {
+    showlegend: false,
+    xaxis: { title: "OTU ID" }
+  };
+
+  Plotly.newPlot("bubble", bubbleData, layout);
+}
+
+init();
